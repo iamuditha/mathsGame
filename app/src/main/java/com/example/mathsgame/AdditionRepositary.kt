6cc8879a04119d4bc9685cbe868d98a02076c020
uditha-anuranjana <udithaxy@gmail.com>
2019-10-28 11:01:06 +0530

    Home Page UI Modification

--- app/src/main/java/com/example/mathsgame/AdditionRepositary.kt
+++ app/src/main/java/com/example/mathsgame/AdditionRepositary.kt
@@ -4,9 +4,9 @@ class AdditionRepositary {
 
     fun fetchAdditionQuiz() : List<Quiz>{
         return listOf(
-            Quiz(1,"2 + 5"),
-            Quiz(1,"4 + 6"),
-            Quiz(1,"12 + 5")
+            Quiz(1,"2+5"),
+            Quiz(1,"4+6"),
+            Quiz(1,"12+5")
             )
     }
 }
